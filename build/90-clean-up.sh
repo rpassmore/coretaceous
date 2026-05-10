0e9b462c709324bdf6cfc2cf3e1f8ab6f1eb9dcc
rpassmore <rpassmore@gmail.com>
2026-05-10 10:27:51 +0100

    Update 90-clean-up.sh

--- build/90-clean-up.sh
+++ build/90-clean-up.sh
@@ -6,6 +6,7 @@ echo "Cleaning up dnf caches..."
 
 # Remove any left over dnf caches
 dnf5 clean all
+rm -rf /var/cache/fontconfig/*
 rm -rf /var/cache/dnf /var/cache/libdnf5
 
 echo "Dnf caches cleaning completed"
