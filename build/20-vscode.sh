195d8ff19bf1559885dac3c78a0c5fbd75f33a22
rpassmore <rpassmore@gmail.com>
2026-05-10 10:38:21 +0100

    Update 20-vscode.sh

--- build/20-vscode.sh
+++ build/20-vscode.sh
@@ -19,6 +19,12 @@ EOF
 # Install vscode
 dnf5 install -y code
 
+# Add VsCode to its own bootc layer
+setfattr -R \
+  -n user.component \
+  -v vscode \
+  /usr/bin/code
+
 # Clean up repo file (required - repos don't work at runtime in bootc images)
 rm -f /etc/yum.repos.d/vs-code.repo
 
