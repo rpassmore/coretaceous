24daf464f3236101eec83274d56d1067ed7cbb07
rpassmore <rpassmore@gmail.com>
2026-05-17 19:14:22 +0100

    Image size optimisations (#125)
    * Reduce layer churn
    
    * Resolve cache misses for layer
    
    * Update 90-clean-up.sh
    
    * try and reduce fetching of the 1.1G layer

--- build/90-clean-up.sh
+++ build/90-clean-up.sh
@@ -8,10 +8,6 @@ dnf5 clean all
 rm -rf /var/cache/fontconfig/*
 rm -rf /var/cache/dnf /var/cache/libdnf5
 
-# Find all container whiteout markers created by your 'dnf5 remove' step
-# and freeze their timestamps to your static base date.
-find /usr /var /etc -name ".wh.*" -exec touch -h -d "2026-01-01 00:00:00" {} +
-
 # Vacuum the database if sqlite3 is present to reduce size changes, then clamp
 if [ -f /usr/share/rpm/rpmdb.sqlite ]; then
   if command -v sqlite3 &> /dev/null; then
